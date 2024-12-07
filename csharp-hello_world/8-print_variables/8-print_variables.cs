1616c88201c5f74df9d644289d70a5345e08e146
gomez7373 <sgc.holberton@gmail.com>
2024-12-07 13:18:03 -0400

    Task's

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-Console.WriteLine("Hello,using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int number = 972;
-        string text = "Mission Street";
-        Console.WriteLine($"{number} {text}");
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine("{2} {1} {0}", str1, str2, int1);
     }
 }
-World!");
