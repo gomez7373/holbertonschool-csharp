96ae9eba58c419e514a1ab11c58327fcb60c93cd
gomez7373 <sgc.holberton@gmail.com>
2024-12-07 13:41:54 -0400

    Task 11 - Create a new sentence

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "C# is an object-oriented programming language.";
+        str = str.Substring(8, 12) + str.Substring(28, 12) + str.Substring(0, 2);
+        Console.WriteLine(str);
+    }
+}
+
