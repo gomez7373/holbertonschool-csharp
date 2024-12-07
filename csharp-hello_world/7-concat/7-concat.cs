e12754079740aa6b4e57b642a4ee4c89d78970f2
gomez7373 <sgc.holberton@gmail.com>
2024-12-07 13:16:28 -0400

    Task's

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -2,10 +2,11 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        string str1 = "Welcome to";
-        string str2 = "Holberton School!";
-        Console.WriteLine($"{str1} {str2}");
-    }
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = str1 + " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
 }
