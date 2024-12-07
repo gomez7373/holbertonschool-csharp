37e1019280086e83d1c88776fad04a0dac988132
gomez7373 <sgc.holberton@gmail.com>
2024-12-07 15:35:55 -0400

    Task

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(-10000, 10000);
+        if (number > 0)
+            Console.WriteLine($"{number} is positive");
+        else if (number < 0)
+            Console.WriteLine($"{number} is negative");
+        else
+            Console.WriteLine($"{number} is zero");
+    }
+}
+
