58c336e6bf038bdd39f1ec7f1e91f51efeef0d8c
gomez7373 <sgc.holberton@gmail.com>
2024-12-08 21:51:09 -0400

    Task

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (i == 8 && j == 9)
+                    Console.WriteLine($"{i}{j}");
+                else
+                    Console.Write($"{i}{j}, ");
+            }
+        }
+    }
+}
