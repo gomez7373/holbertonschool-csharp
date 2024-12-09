4a7e8cf246f39a1b028f853c1f67d68befe499a7
gomez7373 <sgc.holberton@gmail.com>
2024-12-08 23:18:56 -0400

    Complete task 11: Print Diagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine(new string(' ', i) + "\\");
+        }
+    }
+}
