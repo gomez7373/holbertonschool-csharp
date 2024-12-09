816819c03a936b696ba7fcafbcd87568319bd7a6
gomez7373 <sgc.holberton@gmail.com>
2024-12-08 21:31:41 -0400

    Task

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int number = 0; number <= 99; number++)
+        {
+            if (number == 99)
+            {
+                Console.Write($"{number:D2}\n");
+            }
+            else
+            {
+                Console.Write($"{number:D2}, ");
+            }
+        }
+    }
+
+}
