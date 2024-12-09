924445e1284e4906f85478c25774ad4b00257246
gomez7373 <sgc.holberton@gmail.com>
2024-12-09 13:04:56 -0400

    Task

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array = new int[size];
+
+        for (int x = 0; x < size; x++)
+        {
+            array[x] = x;
+            Console.Write(x);
+
+            if (x < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+        return array;
+    }
+}
