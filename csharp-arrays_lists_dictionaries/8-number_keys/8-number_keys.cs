d56949ebb6d8a3af141af598f15e3de2b4f6daa9
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 16:08:49 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static int NumberOfKeys(Dictionary<string, string> myDict)
+	{
+		int count = 0;
+
+		foreach (var i in myDict)
+		{
+			count += 1;
+		}
+		return count;
+	}
+}
