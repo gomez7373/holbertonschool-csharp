c7d49454c04297edb8f6c7dd9df04634158f718f
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 17:10:31 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return "None";
+
+        KeyValuePair<string, int> best = myList.OrderByDescending(item => item.Value).First();
+        return best.Key;
+    }
+}
