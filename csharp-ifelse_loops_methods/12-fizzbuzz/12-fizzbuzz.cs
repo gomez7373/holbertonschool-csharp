14f2ec66ca9e0e075e3f727e09772b27abddd97f
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 18:14:46 -0400

    task

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,20 +1,24 @@
-using System;
+﻿using System;
 
 class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         for (int i = 1; i <= 100; i++)
         {
-            if (i % 3 == 0 && i % 5 == 0)
-                Console.Write("FizzBuzz ");
+            if ((i % 3 == 0) && (i % 5 == 0))
+                Console.Write("FizzBuzz");
             else if (i % 3 == 0)
-                Console.Write("Fizz ");
+                Console.Write("Fizz");
             else if (i % 5 == 0)
-                Console.Write("Buzz ");
+                Console.Write("Buzz");
             else
-                Console.Write(i + " ");
+                Console.Write(i);
+
+            if (i < 100)
+                Console.Write(" ");
+            else
+                Console.WriteLine("");
         }
-        Console.WriteLine();
     }
 }
