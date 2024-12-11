221505739f5fd57e8832410caff889816a1af89b
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 17:08:36 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            foreach (var entry in myDict.OrderBy(entry => entry.Key))
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+    }
