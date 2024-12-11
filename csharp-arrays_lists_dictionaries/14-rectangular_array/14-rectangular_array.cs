cf282431446a14450532db45e5c4e9aae4e32f60
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 17:12:35 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] arrayOfNumbers = new int[5, 5];
+
+        for (int i= 0; i < 5; i++)
+        {
+            for (int j=0; j < 5; j++)
+                arrayOfNumbers[i, j] = 0;
+        }
+
+        arrayOfNumbers[2, 2] = 1;
+
+        for (int i= 0; i < 5; i++)
+        {
+            for (int j=0; j < 5; j++)
+                Console.Write("{0}{1}", arrayOfNumbers[i, j], j < 4 ? " ": "");
+            Console.WriteLine();
+        }
+    }
+}
