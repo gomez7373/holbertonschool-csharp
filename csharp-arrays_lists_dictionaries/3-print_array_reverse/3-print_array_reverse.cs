7c501f384d326e64bf0a0dadf3a363b6dfc47a92
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 14:57:45 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int x = array.Length - 1; x >= 0; x--)
+        {
+            Console.Write(array[x]);
+            if (x > 0)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
