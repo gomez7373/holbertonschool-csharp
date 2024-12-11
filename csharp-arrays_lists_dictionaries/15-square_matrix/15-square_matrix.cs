ee7acc4be4bd930c153535b21ca2f403339ebec3
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 17:17:31 -0400

    task

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int i, y;
+        int[,] square = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
+
+        for (i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            for (y = 0; y < myMatrix.GetLength(1); y++)
+                square[i,y] = myMatrix[i,y] * myMatrix[i,y]; 
+        }
+
+        return square;
+    }
+}
