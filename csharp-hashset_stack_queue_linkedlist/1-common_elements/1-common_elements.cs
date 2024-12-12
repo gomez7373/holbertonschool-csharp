841b3ca1441e4a94eb5174f2f3649a344c14b6da
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 20:00:29 -0400

    task

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> listset = new List<int>();
+
+        list1.Sort();
+        list2.Sort();
+
+        for (int i = 0, j = 0; i < list1.Count && j < list2.Count; )
+        {
+            if (list1[i] == list2[j])
+            {
+                listset.Add(list1[i]);
+                i++;
+                j++;
+            }
+            else if (list1[i] > list2[j])
+                j++;
+            else
+                i++;
+        }
+        return listset;
+    }
+}
