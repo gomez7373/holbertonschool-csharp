7737979dcc059c7b327b0f87e2b9d70da81da992
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 20:33:45 -0400

    task

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int sum = aQueue.Count;
+
+        Console.WriteLine("Number of items: " + sum);
+
+        if (sum == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: " + aQueue.Peek());
+
+        aQueue.Enqueue(newItem);
+
+        bool search_result = aQueue.Contains(search);
+
+        Console.WriteLine("Queue contains \"" + search + "\": " + search_result);
+
+        if (search_result)
+        {
+            string temp = "";
+
+            while (temp != search)
+                temp = aQueue.Dequeue();
+        }
+
+        return aQueue;
+    }
+}
