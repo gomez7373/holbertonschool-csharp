11d8e350bc0d5349dd61fbef6b579b02acb9a6a1
gomez7373 <sgc.holberton@gmail.com>
2024-12-11 20:18:25 -0400

    task

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,32 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int sum = aStack.Count;
+        bool search_result = aStack.Contains(search);
+
+        Console.WriteLine("Number of items: " + sum);
+
+        if (sum == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: " + aStack.Peek());
+
+        Console.WriteLine("Stack contains \"" + search + "\": " + search_result);
+
+        if (search_result)
+        {
+            string temp = "";
+
+            while (temp != search)
+                temp = aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
