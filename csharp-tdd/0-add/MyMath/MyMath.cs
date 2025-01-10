6f285f5e1d0e4ee985fb253065065cfdaf5913b1
gomez7373 <sgc.holberton@gmail.com>
2025-01-09 23:15:28 -0400

    tasks

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,19 @@
+namespace MyMath
+{
+    /// <summary>
+    /// Provides basic mathematical operations.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Adds two integers.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>Sum of a and b.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
