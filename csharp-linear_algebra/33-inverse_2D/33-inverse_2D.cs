3e674b788fc740fac7971987017eaf629f0ee147
gomez7373 <sgc.holberton@gmail.com>
2025-01-30 18:35:33 -0400

    tasks

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -4,30 +4,20 @@ class MatrixMath
 {
     public static double[,] Inverse2D(double[,] matrix)
     {
-        // Ensure the matrix is 2x2
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-        {
-            return new double[,] { { -1 } };
-        }
+            return new double[,] {{-1}};
 
-        // Calculate the determinant
-        double determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        double det = matrix[0,0] * matrix[1,1] - matrix[1,0] * matrix[0,1];
 
-        // If the determinant is 0, the matrix is non-invertible
-        if (determinant == 0)
-        {
-            return new double[,] { { -1 } };
-        }
+        if (det == 0)
+            return new double[,] {{-1}};
 
-        // Calculate the inverse
-        double inverseDet = 1 / determinant;
-        double[,] inverse = new double[2, 2];
+        double[,] invert = new double[,] {{matrix[1,1] / det, -matrix[0,1] / det}, {-matrix[1,0] / det, matrix[0,0] / det}};
 
-        inverse[0, 0] = inverseDet * matrix[1, 1];
-        inverse[0, 1] = -inverseDet * matrix[0, 1];
-        inverse[1, 0] = -inverseDet * matrix[1, 0];
-        inverse[1, 1] = inverseDet * matrix[0, 0];
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                invert[i, j] = Math.Round(invert[i, j], 2);
 
-        return inverse;
+        return invert;
     }
 }
