da4370b3b425c65db086f101ab4207c9e8dfbd96
gomez7373 <sgc.holberton@gmail.com>
2025-01-30 19:48:29 -0400

    tasks

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int colsB = matrix2.GetLength(1);
+
+        if (cols != matrix2.GetLength(0))
+            return new double[,] {{-1}};
+
+        double[,] result = new double[rows, colsB];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < colsB; j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < cols; k++)
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+
+        return result;
+
+    }
+}
