1f29d4bd4e595d8bf51ac5e6cf2e09676c5b99f7
gomez7373 <sgc.holberton@gmail.com>
2025-01-30 19:26:11 -0400

    tasks

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
+            return -1;
+
+        double sum = 0;
+
+        for (int i = 0; i < vector1.Length; i++)
+            sum += vector1[i] * vector2[i];
+
+        return sum;
+    }
+}
