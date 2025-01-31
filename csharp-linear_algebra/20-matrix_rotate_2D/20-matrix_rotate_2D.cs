90671ce0432c7abd9d35d6b40a7436281ae47321
gomez7373 <sgc.holberton@gmail.com>
2025-01-30 20:17:38 -0400

    tasks

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2))
+            return new double[,] {{-1}};
+
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+        double[,] rotM;
+
+        rotM = new double[,] {{ cos, sin },{ -sin, cos } };
+
+        return Multiply(matrix, rotM);
+    }
+
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int colsB = matrix2.GetLength(1);
+
+        if (cols != matrix2.GetLength(0))
+            return new double[,] {{-1}};
+
+        double[,] result = new double[rows, colsB];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < colsB; j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < cols; k++)
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                result[i, j] = Math.Round(result[i, j], 2);
+            }
+
+        return result;
+
+    }
+}
---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (direction != 'x' && direction != 'y'))
+            return new double[,] {{-1}};
+
+        int k = 0;
+        double[,] result = new double[2,2];
+
+        if (direction == 'y')
+            k = 1;
+
+        for (int i = 0; i < 2; i++)
+        {
+            result[i, 0] = matrix[i, 0];
+            result[i, 1] = matrix[i, 1];
+            result[i, k] += matrix[i, 1 - k] * factor;
+        }
+
+
+        return result;
+    }
+}
