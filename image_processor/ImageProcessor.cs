841d9a40c559afef523f06eaa8cdaca438556e6b
gomez7373 <sgc.holberton@gmail.com>
2025-06-15 15:04:37 -0400

    task 0

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,54 +1,131 @@
-using System;
-using System.Drawing; // Para trabajar con imágenes
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using System.Threading.Tasks; // Para usar multihilos modernos (Task)
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-public class ImageProcessor
+namespace image_processor
 {
-    /// <summary>
-    /// Inverts the colors of each image in the filenames array.
-    /// </summary>
-    public static void Inverse(string[] filenames)
+    class ImageProcessor
     {
-        Parallel.ForEach(filenames, file =>
+        private static string[] _filenames;
+
+        /// <summary>
+        /// Runs a Color invert process on a list of images.
+        /// </summary>
+        /// <param name="filenames">List of files</param>
+        public static void Inverse(string[] filenames)
         {
-            try
+            Console.WriteLine(filenames[0]);
+            _filenames = filenames;
+            Thread.CurrentThread.Name = "Main";
+            List<Task> tasks = new List<Task>();
+            // for (int i = 0; i < tasks.Length; i++)
+            // {
+            //     var i1 = i;
+            //     tasks[i] = Task.Factory.StartNew(() =>
+            //         InvertTask(filenames[i1])
+            //     );
+            // }
+
+            foreach (var filename in filenames)
             {
-                using (Bitmap image = new Bitmap(file))
+                // tasks.Add(Task.Run(() => InvertTask(filename)));
+                tasks.Add(Task.Run(() => MatrixInvertTask(filename)));
+            }
+
+            Console.WriteLine("Starting Tasks");
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Finished Tasks");
+        }
+
+
+        private static void Invert()
+        {
+            foreach (var filename in _filenames)
+            {
+                Console.WriteLine($"Processing: {filename}");
+                Image img = Image.FromFile(filename);
+
+                Bitmap bitmap = new Bitmap(img);
+                Bitmap newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+                for (int x = 0; x < bitmap.Width; x++)
+                for (int y = 0; y < bitmap.Height; y++)
                 {
-                    // Recorre cada píxel
-                    for (int y = 0; y < image.Height; y++)
-                    {
-                        for (int x = 0; x < image.Width; x++)
-                        {
-                            Color original = image.GetPixel(x, y);
-
-                            // Invertir cada canal de color (255 - valor)
-                            Color inverted = Color.FromArgb(
-                                original.A,
-                                255 - original.R,
-                                255 - original.G,
-                                255 - original.B
-                            );
-
-                            image.SetPixel(x, y, inverted);
-                        }
-                    }
-
-                    // Crear el nuevo nombre del archivo
-                    string directory = Path.GetDirectoryName(file);
-                    string filename = Path.GetFileNameWithoutExtension(file);
-                    string extension = Path.GetExtension(file);
-
-                    string newPath = Path.Combine(directory ?? "", $"{filename}_inverse{extension}");
-
-                    image.Save(newPath);
+                    var pixel = bitmap.GetPixel(x, y);
+                    pixel = Color.FromArgb(pixel.ToArgb() ^ 0xffffff);
+                    newBitmap.SetPixel(x, y, pixel);
                 }
+
+                var fn = Path.GetFileNameWithoutExtension(filename);
+                var ext = Path.GetExtension(filename);
+                var file = $"images/{fn}_inverse.{ext}";
+                newBitmap.Save(file);
+                // var e = img.GetEncoderParameterList(Guid.Empty);
+                // Console.WriteLine($"{e.Param}");
             }
-            catch (Exception ex)
+        }
+
+        private static void InvertTask(string filename)
+        {
+            Console.WriteLine($"Processing: {filename}");
+            Image img = Image.FromFile(filename);
+            Bitmap bitmap = new Bitmap(img);
+
+            Bitmap newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+            for (int x = 0; x < bitmap.Width; x++)
+            for (int y = 0; y < bitmap.Height; y++)
             {
-                Console.WriteLine($"Error processing {file}: {ex.Message}");
+                var pixel = bitmap.GetPixel(x, y);
+                pixel = Color.FromArgb(pixel.ToArgb() ^ 0xffffff);
+                newBitmap.SetPixel(x, y, pixel);
             }
-        });
+
+            var fn = Path.GetFileNameWithoutExtension(filename);
+            var ext = Path.GetExtension(filename);
+            var file = $"images/{fn}_inverse{ext}";
+            newBitmap.Save(file, img.RawFormat);
+        }
+
+        private static void MatrixInvertTask(string filename)
+        {
+            Console.WriteLine($"Processing: {filename}");
+            Image img = Image.FromFile(filename);
+            Bitmap bitmap = new Bitmap(img);
+
+            Graphics g = Graphics.FromImage(bitmap);
+            
+            ColorMatrix clrMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] {-1, 0, 0, 0, 0},
+                    new float[] {0, -1, 0, 0, 0},
+                    new float[] {0, 0, -1, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {1, 1, 1, 0, 1}
+                });
+
+            ImageAttributes attributes = new ImageAttributes();
+
+            attributes.SetColorMatrix(clrMatrix);
+
+            g.DrawImage(img,
+                new Rectangle(0, 0, img.Width, img.Height),
+                0, 0,
+                img.Width, img.Height,
+                GraphicsUnit.Pixel,
+                attributes
+            );
+            g.Dispose();
+
+            var fn = Path.GetFileNameWithoutExtension(filename);
+            var ext = Path.GetExtension(filename);
+            bitmap.Save($"./{fn}_inverse{ext}", img.RawFormat);
+        }
     }
 }
