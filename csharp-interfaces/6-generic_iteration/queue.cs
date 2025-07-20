c3f098295406980e1aa2b39469a17cdd64ec1060
gomez7373 <sgc.holberton@gmail.com>
2025-07-19 22:29:37 -0400

    Completed task: 6-generic_iteration

--- csharp-interfaces/6-generic_iteration/queue.cs
+++ csharp-interfaces/6-generic_iteration/queue.cs
@@ -2,33 +2,100 @@
 using System.Collections;
 using System.Collections.Generic;
 
-/// <summary>
-/// Objs generic class that implements IEnumerable<T>
-/// </summary>
-/// <typeparam name="T">The type parameter</typeparam>
-class Objs<T> : IEnumerable<T>
+/// <summary>Abstract base class</summary>
+public abstract class Base
 {
-    List<T> collection = new List<T>();
+    /// <summary>Name of the object</summary>
+    public string name { get; set; }
 
-    /// <summary>
-    /// Adds an object to the list
-    /// </summary>
+    /// <summary>Returns string representation of the object</summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>Interface for interactive objects</summary>
+public interface IInteractive
+{
+    /// <summary>Interact method</summary>
+    void Interact();
+}
+
+/// <summary>Class representing a door</summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>Constructor for Door</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    /// <summary>Interact with the Door</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+/// <summary>Class representing a decoration</summary>
+public class Decoration : Base, IInteractive
+{
+    /// <summary>Durability of the decoration</summary>
+    public int durability { get; set; }
+
+    /// <summary>Is a quest item?</summary>
+    public bool isQuestItem;
+
+    /// <summary>Constructor</summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+            throw new ArgumentException("Durability must be greater than 0");
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    /// <summary>Interact with the decoration</summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+            Console.WriteLine($"The {name} has been broken.");
+        else if (isQuestItem)
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        else
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+    }
+
+    /// <summary>Break the decoration</summary>
+    public void Break()
+    {
+        durability--;
+        if (durability > 0)
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        else
+            Console.WriteLine($"You smash the {name}. What a mess.");
+    }
+}
+
+/// <summary>Generic iterable collection</summary>
+public class Objs<T> : IEnumerable<T>
+{
+    private List<T> _objects = new List<T>();
+
+    /// <summary>Adds an item</summary>
     public void Add(T obj)
     {
-        collection.Add(obj);
+        _objects.Add(obj);
     }
 
-    /// <summary>
-    /// Returns an enumerator that iterates through the collection
-    /// </summary>
+    /// <summary>Returns enumerator</summary>
     public IEnumerator<T> GetEnumerator()
     {
-        return collection.GetEnumerator();
+        return _objects.GetEnumerator();
     }
 
-    /// <summary>
-    /// Non-generic version required by IEnumerable
-    /// </summary>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return this.GetEnumerator();
